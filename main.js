25a184e15d629c2d36e2aff8060fb30123599ff9
Dardiuz <donnynoorlander@gmail.com>
2023-05-13 12:22:38 +0200

    added: functionality to the buttons. black button will give a black drawing pen random button will give a drawing pen with random colors reset button will reset the grid field

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const container = document.querySelector(".container");
 
+let color = "black";
+let click = true;
+
 //creating the grid field
 const createGrid = (size) => {
   for (let i = 0; i < size; i++) {
@@ -10,9 +13,30 @@ const createGrid = (size) => {
     for (let j = 0; j < size; j++) {
       const box = document.createElement("div");
       box.classList.add("grid-box");
+      box.addEventListener("mouseenter", drawingPen);
       row.appendChild(box);
     }
   }
 };
 
 createGrid(16);
+
+function drawingPen() {
+  if (click) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  const container = document.querySelector(".container");
+  let row = container.querySelectorAll("div");
+  row.forEach((div) => (div.style.backgroundColor = "white"));
+}
