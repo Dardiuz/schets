b9b580a4c41e6ed6caa540f8d43f1be32d2143f6
Dardiuz <donnynoorlander@gmail.com>
2023-05-15 20:42:01 +0200

    added: input button

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+let popup = document.querySelector("#popupButton");
 
 let color = "black";
 let click = true;
@@ -18,6 +19,25 @@ const createGrid = (size) => {
     }
   }
 };
+
+popup.addEventListener("click", function () {
+  let size = getSize();
+  container.replaceChildren();
+  createGrid(size);
+});
+
+function getSize() {
+  let input = prompt("Give the Size");
+  let message = document.querySelector("#message");
+  if (input == "") {
+    message.innerHTML = "Please provide a number";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Please provide a number between 1 and 100";
+  } else {
+    message.innerHTML = "the field is created! you can start drawing!";
+    return input;
+  }
+}
 
 createGrid(16);
 
